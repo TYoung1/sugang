4eeb157ff273a3c21f29759cb58b5dd74f9e9cbb
TYoung1 <lail_mind@naver.com>
2023-07-14 20:37:44 +0900

    sugang

---
+++ tichimi/src/main/java/sugang/tichimi/TichimiApplication.java
@@ -0,0 +1,13 @@
+package sugang.tichimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TichimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TichimiApplication.class, args);
+	}
+
+}
---
+++ tichimi/src/test/java/sugang/tichimi/TichimiApplicationTests.java
@@ -0,0 +1,13 @@
+package sugang.tichimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TichimiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
